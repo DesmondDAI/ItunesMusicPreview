79215a6b11142b280d579605c02bf8869c6f36ec
DesmondDAI <dxmlore@gmail.com>
2017-10-22 12:26:33 +0800

    [New] Add basic layout for search page

---
+++ ItunesMusicPreview/Viewcontrollers/MusicTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MusicTableViewController.swift
+//  ItunesMusicPreview
+//
+//  Created by DAIXinming on 22/10/2017.
+//  Copyright © 2017 Xinming DAI. All rights reserved.
+//
+
+import UIKit
+
+class MusicTableViewController: UIViewController {
+
+    @IBOutlet weak var searchTextFieldContainerView: UIView!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchCancelBtn: UIButton!
+    @IBOutlet weak var searchTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ItunesMusicPreview/Views/SearchHistoryTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SearchHistoryTableViewCell.swift
+//  ItunesMusicPreview
+//
+//  Created by DAIXinming on 22/10/2017.
+//  Copyright © 2017 Xinming DAI. All rights reserved.
+//
+
+import UIKit
+
+class SearchHistoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var historyLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
